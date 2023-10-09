a44df380fe299d994b8850d7c11fb46fbf67a2e5
Harsh <harshpatel1632@gmail.com>
2023-10-08 18:00:10 -0600

    Added activation function

--- network.py
+++ network.py
@@ -1,12 +1,15 @@
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
 
-#Initiale random generator
-np.random.seed(0)
+nnfs.init()
 
 #Sample input data
-X = [[1.0, 2.0, 3.0, 2.5],
-    [2.0, 5.0, -1.0, 2.0],
-    [-1.5, 2.7, 3.3, -0.8]]
+# X = [[1.0, 2.0, 3.0, 2.5],
+#     [2.0, 5.0, -1.0, 2.0],
+#     [-1.5, 2.7, 3.3, -0.8]]
+
+X, y = spiral_data(100,3)
 
 class Dense_Layer:
     def __init__(self, n_inputs, n_neurons):
@@ -18,11 +21,21 @@ class Dense_Layer:
     def foward_pass(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.baises
 
-#Initialize the neural network layers
-layer1 = Dense_Layer(4,5)
-layer2 = Dense_Layer(5,2)
 
+class ReLU_Activation:
+    def forward_pass(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+#Initialize the neural network layers & activation func
+layer1 = Dense_Layer(2,5)
+#layer2 = Dense_Layer(5,2)
+activation1 = ReLU_Activation()
+
+
+#Forward pass inputs through network layers & activation func
 layer1.foward_pass(X)
-#print(layer1.output)
-layer2.foward_pass(layer1.output)
-print(layer2.output)
+print(layer1.output)
+#layer2.foward_pass(layer1.output)
+#print(layer2.output)
+activation1.forward_pass(layer1.output)
+print(activation1.output)
